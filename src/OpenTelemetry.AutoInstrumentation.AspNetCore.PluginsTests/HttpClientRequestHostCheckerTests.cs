55d61edbc259a8e7fb5aea490e20562f3a24a856
YuChia <h42279@gmail.com>
2025-12-04 22:30:28 +0800

    convert test project to xUnit  (by Junie Pro)

--- src/OpenTelemetry.AutoInstrumentation.AspNetCore.PluginsTests/HttpClientRequestHostCheckerTests.cs
+++ src/OpenTelemetry.AutoInstrumentation.AspNetCore.PluginsTests/HttpClientRequestHostCheckerTests.cs
@@ -1,20 +1,16 @@
-using Microsoft.AspNetCore.Http;
 using OpenTelemetry.AutoInstrumentation.AspNetCore.Plugins;
+using Xunit;
 
 namespace OpenTelemetry.AutoInstrumentation.AspNetCore.PluginsTests;
 
-[TestClass]
 public class HttpClientRequestHostCheckerTests
 {
-    [TestMethod]
-    [Owner("YuChia")]
-    [TestCategory(nameof(HttpClientRequestHostChecker))]
-    [TestProperty(nameof(HttpClientRequestHostChecker), nameof(HttpClientRequestHostChecker.IsValidHost))]
-    [DataRow("http://other-service.other-namespace.svc.cluster.local/logo.png", true)]
+    [Theory]
+    [InlineData("http://other-service.other-namespace.svc.cluster.local/logo.png", true)]
     public void 檢查是否為需要啟動追蹤的路徑(string host, bool expected)
     {
         var uri = new Uri(host);
         var actual = HttpClientRequestHostChecker.IsValidHost(uri.Host);
-        Assert.AreEqual(expected, actual);
+        Assert.Equal(expected, actual);
     }
 }
